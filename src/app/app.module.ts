77966ad9d8c7b0880d0735d6b129d5a2da8e28ef
dtopalov <dimiter.topalov79@gmail.com>
2019-11-22 14:21:12 +0200

    chore: initial demo commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MenuModule } from '@progress/kendo-angular-menu';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+import 'hammerjs';
+
+import { HeaderComponent } from './components/header/header.component';
+import { GridComponent } from './components/grid/grid.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'grid', component: GridComponent },
+    { path: 'chart', component: ChartComponent },
+    { path: 'forms', component: FormComponent },
+    { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        GridComponent,
+        ChartComponent,
+        FormComponent,
+        HomeComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MenuModule,
+        BrowserAnimationsModule,
+        GridModule,
+        ChartsModule,
+        RouterModule.forRoot(routes),
+        DropDownsModule, PopupModule, InputsModule
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent {
+
+}
